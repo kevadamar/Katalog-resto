aa1d7cfff62b1698457e2f19a18f3d88ae58c97b
keva damar galih <kevadamarg@gmail.com>
2021-06-11 20:32:49 +0700

    fix z-index & config cacheName

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: `k-resto-${new Date().toISOString()}`,
+  CACHE_NAME: 'k-resto-cache1',
   DATABASE_NAME: 'resto-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restos',
